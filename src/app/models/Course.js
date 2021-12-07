c9623cf0e335d05665155235f0e406e0f24c2c5f
Hoang Phuc <hoangphuckt7@gmail.com>
2021-12-08 00:45:10 +0700

    Auto increment id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 var slug = require("mongoose-slug-generator");
-var mongooseDelete = require('mongoose-delete');
+var mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const Course = new Schema(
   {
-    name: { type: "String", required: true },
-    description: { type: "String" },
-    image: { type: "String" },
-    videoId: { type: "String" },
-    level: { type: "String" },
+    _id: { type: Number },
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String },
+    level: { type: String },
     slug: { type: String, slug: "name", unique: true },
   },
   {
+    _id: false,
     timestamps: true,
   }
 );
 
-
 //All plugins
 mongoose.plugin(slug);
-Course.plugin(mongooseDelete, { 
-  deletedAt : true,
-  overrideMethods: 'all',
+Course.plugin(AutoIncrement);
+Course.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: "all",
 });
 
 module.exports = mongoose.model("Course", Course);
